0c4e7ffa85363057b2527edfb0a8810833f1a646
Anna-Anka <anjbekrenewa@gmail.com>
2024-08-15 13:25:56 +0300

    add smooth-scroll

--- src/js/_include.js
+++ src/js/_include.js
@@ -9,3 +9,27 @@ new GraphModal('question');
 // burger
 import {burger} from './templates/_burger.js'
 burger()
+
+// smooth scroll
+// const links = document.querySelectorAll('.header__nav a')
+// links.forEach((link) => {
+//     link.addEventListener('click', (e) => {
+//         e.preventDefault()
+//     })
+// })
+
+import SmoothScroll from '../../node_modules/smooth-scroll/dist/smooth-scroll.polyfills.min.js'
+
+// const scroll = new SmoothScroll('a[data-anchor]', {
+//     header: '.header',
+//     clip: true
+// });
+
+// const links = document.querySelectorAll('a[href*="#"]');
+
+// links.forEach((link) => {
+//     link.addEventListener('click', (e) => {
+//         e.preventDefault();
+//         scroll.animateScroll(link.getAttribute('data-anchor'));
+//     });
+// });
